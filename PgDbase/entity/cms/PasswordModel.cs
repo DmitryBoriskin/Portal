9e2490748feb9676333e31d2756177ad7b033aea
travis88 <hellsballs99@gmail.com>
2018-04-26 15:36:03 +0300

    восстановление пароля пользователя

---
+++ PgDbase/entity/cms/PasswordModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PgDbase.entity.cms
+{
+    /// <summary>
+    /// Пароль
+    /// </summary>
+    public class PasswordModel
+    {
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        [Required(ErrorMessage = "Поле Пароль» не должно быть пустым.")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 16 символов")]
+        [RegularExpression(@"(?=.*\d)(?=.*[A-Za-z]).{6,16}", ErrorMessage = "Пароль имеет неправильный формат")]
+        [DataType(DataType.Password)]
+        public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Подтверждение пароля
+        /// </summary>
+        [Required(ErrorMessage = "Поле «Подтверждение пароля» не должно быть пустым.")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 16 символов")]
+        [RegularExpression(@"(?=.*\d)(?=.*[A-Za-z]).{6,16}", ErrorMessage = "Подтверждение пароля имеет неправильный формат")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public virtual string PasswordConfirm { get; set; }
+    }
+}
--- PgDbase/entity/cms/UserModel.cs
+++ PgDbase/entity/cms/UserModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace PgDbase.entity.cms
 {
@@ -73,30 +72,5 @@ namespace PgDbase.entity.cms
                 return $"{Surname} {Name} {Patronimyc}";
             }
         }
-    }
-
-    /// <summary>
-    /// Пароль
-    /// </summary>
-    public class PasswordModel
-    {
-        /// <summary>
-        /// Пароль
-        /// </summary>
-        [Required(ErrorMessage = "Поле Пароль» не должно быть пустым.")]
-        [StringLength(16, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 16 символов")]
-        [RegularExpression(@"(?=.*\d)(?=.*[A-Za-z]).{6,16}", ErrorMessage = "Пароль имеет не правильный формат")]
-        [DataType(DataType.Password)]
-        public virtual string Password { get; set; }
-
-        /// <summary>
-        /// Подтверждение пароля
-        /// </summary>
-        [Required(ErrorMessage = "Поле «Подтверждение пароля» не должно быть пустым.")]
-        [StringLength(16, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 16 символов")]
-        [RegularExpression(@"(?=.*\d)(?=.*[A-Za-z]).{6,16}", ErrorMessage = "Подтверждение пароля имеет не правильный формат")]
-        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
-        [DataType(DataType.Password)]
-        public virtual string PasswordConfirm { get; set; }
     }
 }
