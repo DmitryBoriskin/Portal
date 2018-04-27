4eb48f7a20404c58da5eb462df0925abea5317ce
ailo21 <alex92ailo@mail.ru>
2018-04-27 09:44:16 +0300

    domain list

--- Portal/Areas/Admin/Models/SitesViewModel.cs
+++ Portal/Areas/Admin/Models/SitesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PgDbase.entity;
+using PgDbase.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,41 +8,21 @@ using System.Web.Mvc;
 
 namespace Portal.Areas.Admin.Models
 {
+    /// <summary>
+    /// модель представления для представления
+    /// </summary>
     public class SitesViewModel : CoreViewModel
     {
         /// <summary>
-        /// Список сайтов
+        /// Постраничный список
         /// </summary>
-        public SitesList List { get; set; }
+        public PagedEnumerable<SitesModel> List { get; set; }
 
         /// <summary>
-        /// Единичная запись сайта
+        /// Сайт
         /// </summary>
         public SitesModel Item { get; set; }
 
-        /// <summary>
-        /// Список типов
-        /// </summary>
-        public SelectList TypeList { get; set; }
 
-        /// <summary>
-        /// Список организаций
-        /// </summary>
-        public SelectList OrgsList { get; set; }
-
-        /// <summary>
-        /// Список событий
-        /// </summary>
-        public SelectList EventsList { get; set; }
-
-        /// <summary>
-        /// Список людей
-        /// </summary>
-        public SelectList MainSpecialistList { get; set; }
-
-        /// <summary>
-        /// Список тем
-        /// </summary>
-        public SelectList Themes { get; set; }        
     }
 }
