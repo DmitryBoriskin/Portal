0e3f1bc7c701453dd3441d4dfea5edbaf367c822
ailo21 <alex92ailo@mail.ru>
2018-05-07 15:12:29 +0300

    news 2

---
+++ PgDbase/entity/cms/NewsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgDbase.Entity
+{
+    public class NewsModel
+    {
+        /// <summary>
+        /// Идентификатор в виде порядкого номера
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Идентификатор в бд
+        /// </summary>
+        public Guid Guid { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Photo { get; set; }
+        public string Keyw { get; set; }
+        public string Desc { get; set; }
+        public string SourceUrl { get; set; }
+        public string SourceName { get; set; }
+        public int ViewCount { get; set; }
+        public bool Disabled { get; set; }
+        public bool Important { get; set; }        
+    }
+
+
+}
