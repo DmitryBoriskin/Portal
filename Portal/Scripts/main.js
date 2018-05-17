4ac1db52014246da29f430fc3934160866afbfa7
ailo21 <alex92ailo@mail.ru>
2018-05-17 11:04:42 +0300

    gallery

---
+++ Portal/Scripts/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    if ($('.gallery').length > 0) {
+        $('.gallery').each(function () {
+            var $ParentBlock = $(this);
+            var url = '/core/GetPhotoGallery/' + $ParentBlock.attr('data-id');            
+            $.getJSON(url, function (data) {
+                for (var i in data) {
+                    var $skipwr = $("<div/>", { "class": "swip_wr" });
+                    var $skipwr1 = $("<div/>", { "class": "swip_wr1" });
+                    var $a = $("<a/>",
+                        { "class": "swipebox" ,
+                         "data-original": data[i].c_url ,
+                         "title": data[i].c_title ,
+                         "style": "background-image:url(" + data[i].c_preview+');' ,
+                         "href": "data[i].c_url" }
+                    );
+                    $skipwr1.append($a);
+                    $skipwr.append($skipwr1);
+                    $ParentBlock.append($skipwr);
+                }              
+            });
+
+            $(".swipebox").swipebox();
+        });
+    }
+});
+
+
