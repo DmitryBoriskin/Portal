27a1e74d00ff739bd2e460353dc1210c5353b754
travis88 <hellsballs99@gmail.com>
2018-05-21 08:38:03 +0300

    front vacancy

---
+++ VacancyModule/Areas/Vacancy/Controllers/VacancyController.cs
@@ -0,0 +1,21 @@
+﻿using PgDbase.entity;
+using Portal.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VacancyModule.Areas.Vacancy.Controllers
+{
+    public class VacancyController : CoreController
+    {
+        // GET: Vacancy/Vacancy
+        public ActionResult Index()
+        {
+            FilterModel filter = GetFilter();
+            _Repository.GetVacancies(filter);
+            return View();
+        }
+    }
+}
