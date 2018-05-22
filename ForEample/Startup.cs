719f8106b0c0014345340fe449409116108ef948
hrum-hrum <svetlana.kaznacheeva@gmail.com>
2018-05-22 14:28:25 +0300

    packages

---
+++ ForEample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForEample.Startup))]
+namespace ForEample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
