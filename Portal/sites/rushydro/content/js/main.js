7e25bd2a3059681875e2611d3714999939b60f84
ailo21 <alex92ailo@mail.ru>
2018-08-07 16:22:06 +0300

    Платежи

--- Portal/sites/rushydro/content/js/main.js
+++ Portal/sites/rushydro/content/js/main.js
@@ -19,3 +19,9 @@
 function getBodyScrollTop() {
     return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);
 }
+//определяет ссылку страницы
+function UrlPage() {
+    var url = location.href;
+    var site = url.split('?');
+    return site[0];
+}
